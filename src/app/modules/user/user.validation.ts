bb50ed3b395761fe3f743926badfe159f6c325a0
riasat01 <riasat01@outlook.com>
2024-12-25 09:15:54 +0600

    feat: add user validation schema - Implemented user validation schema for data integrity. - Ensured proper validation rules and error handling. - Integrated schema with existing user model.

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { UserRoles } from "./user.constant";
+
+const userCreateValidationSchema = z.object({
+    body: z.object({
+        email: z.string().email({ message: `Invalid email address` }),
+        role: z.enum([...UserRoles] as [string, ...string[]]),
+        firebaseUID: z.string({
+            required_error: `Firebase ID is required`,
+            invalid_type_error: `Firebase ID must be a string`,
+        }),
+        name: z.string({
+            required_error: `Name is required`,
+            invalid_type_error: `Name must be a string`,
+        }),
+        phone: z.string().optional(),
+    }),
+});
+
+export const UserValidations = {
+    userCreateValidationSchema,
+};
